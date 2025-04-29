ebabd8e24bb5b479c1ddb2339d81c0b8c0815aa0
prashantpandeygit <prashantpandeyiuet@gmail.com>
2025-04-29 10:07:56 +0530

    updates

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://xpersonalitypredictor.vercel.app/"],  
+    allow_origins=["https://xpersonalitypredictor.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
