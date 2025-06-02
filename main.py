eb6d66ca3e262504043311ca86f1196d14d383a3
Prashant Pandey <132379659+prashantpandeygit@users.noreply.github.com>
2025-06-02 11:11:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://xpersonalitypredictor.vercel.app"],  
+    allow_origins=["https://twitype.onrender.com"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
